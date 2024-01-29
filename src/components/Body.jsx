39c607cc3a6661e453bc2e436f678b69fa7e14ba
nryn0021 <80402194+nryn0021@users.noreply.github.com>
2024-01-30 00:04:52 +0530

    functionality added
    functionality added

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -4,7 +4,7 @@ import { AiFillClockCircle } from "react-icons/ai";
 import { useStateProvider } from "../Utils/StateProvider";
 import axios from "axios";
 import { reducerCases } from "../Utils/Constants";
-export default function Body() {
+export default function Body({ headerBackground }) {
   const [{ token, selectPlaylistId, selectPlaylist }, dispatch] =
     useStateProvider();
   useEffect(() => {
@@ -41,10 +41,15 @@ export default function Body() {
     getInitialPlaylist();
   }, [token, dispatch, selectPlaylistId]);
 
+const msToMinAndSec = (ms) => {
+  const minutes = Math.floor(ms/60000);
+  const seconds = ((ms % 60000) / 1000).toFixed(0);
+  return minutes + ":" + (seconds < 10 ? "0": "") + seconds;
+};
   return (
-    <Container>
+    <Container headerBackground={headerBackground}>
       {selectPlaylist && (
-        <>
+        <div>
           <div className="playlist">
             <div className="image">
               <img src={selectPlaylist.image} alt="selectplaylist" />
@@ -72,51 +77,119 @@ export default function Body() {
                 </span>
               </div>
             </div>
-          </div>
-          <div className="tracks">
-            {selectPlaylist.tracks.map(
-              (
-                {
-                  id,
-                  name,
-                  artists,
-                  image,
-                  duration,
-                  album,
-                  context_uri,
-                  track_number,
-                },
-                index
-              ) => {
-                return (
-                  <div className="row" key={id}>
-                    <div className="col">
-                      <span>{index + 1}</span>
-                    </div>
-                    <div className="col detail">
-                      <div className="image">
-                        <img src={image} alt="track" />
+            <div className="tracks">
+              {selectPlaylist.tracks.map(
+                (
+                  {
+                    id,
+                    name,
+                    artists,
+                    image,
+                    duration,
+                    album,
+                    context_uri,
+                    track_number,
+                  },
+                  index
+                ) => {
+                  return (
+                    <div className="row" key={id}>
+                      <div className="col">
+                        <span>{index + 1}</span>
                       </div>
-                      <div className="info">
-                        <span className="name">{name}</span>
-                        <span>{artists}</span>
+                      <div className="col detail">
+                        <div className="image">
+                          <img src={image} alt="track" />
+                        </div>
+                        <div className="info">
+                          <span className="name">{name}</span>
+                          <span>{artists}</span>
+                        </div>
+                      </div>
+                      <div className="col">
+                        <span>{album}</span>
+                      </div>
+                      <div className="col">
+                        <span>{msToMinAndSec(duration)}</span>
                       </div>
                     </div>
-                    <div className="col">
-                      <span>{album}</span>
-                    </div>
-                    <div className="col">
-                      <span>{duration}</span>
-                    </div>
-                  </div>
-                );
-              }
-            )}
+                  );
+                }
+              )}
+            </div>
           </div>
-        </>
+        </div>
       )}
     </Container>
   );
 }
 
-const Container = styled.div``;
+const Container = styled.div`
+.playlist{
+  margin: 0 2rem;
+  display: flex;
+  align-items: center;
+  gap: 2rem;
+  .image{
+    img{
+      height: 15rem;
+      box-shadow: rgba(0,0,0,0.25) 0px 25px 50px 12px;
+    }
+  }
+  .details{
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    color: #e0dede;
+    .title{
+      color: white;
+      font-size: 4rem;
+    }
+  }
+}
+.list{
+  .header__row{
+    display: grid;
+    grid-template-columns: 0.3fr 3fr 2fr 0.1fr;
+    color: #dddcdc;
+    margin: 1rem 0 0 0;
+    position: sticky;
+    top: 15vh;
+    padding: 1rem 3rem;
+    transition: 0.3s ease-in-out;
+    background-color: ${({ headerBackground }) =>
+      headerBackground ? "#000000dc" : "none"};
+    
+  } 
+  .tracks{
+    margin: 0 2rem;
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 5rem;
+    .row {
+      padding: 0.5rem 1rem;
+      display: grid;
+      grid-template-columns: 0.3fr 3.1fr 1.87fr 0.1fr;
+      &:hover{
+        background-color: rgba(0,0,0,0.7);
+      }
+      .col{
+        display: flex;
+        align-items: center;
+        color: #dddcdc;
+        img{
+          height: 40px;
+        }
+      }
+      .detail{
+        display: flex;
+        gap: 1rem;
+        .info{
+          display: flex;
+          flex-direction: column;
+        }
+      }
+    }
+  }
+}
+`;
--- src/components/Spotify.jsx
+++ src/components/Spotify.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 import Sidebar from "./Sidebar";
 import Navbar from "./Navbar";
@@ -9,6 +9,18 @@ import axios from "axios";
 import { reducerCases } from "../Utils/Constants";
 export default function Spotify() {
   const [{ token }, dispatch] = useStateProvider();
+  const bodyRef = useRef();
+  const [navBackground, setNavBackground] = useState(false);
+  const [headerBackground, setHeaderBackground] = useState(false);
+  const bodyScrolled = () => {
+    bodyRef.current.scrollTop >= 30
+      ? setNavBackground(true)
+      : setNavBackground(false);
+
+    bodyRef.current.scrollTop >= 268
+      ? setHeaderBackground(true)
+      : setHeaderBackground(false);
+  };
   useEffect(() => {
     const getUserInfo = async () => {
       const { data } = await axios.get("https://api.spotify.com/v1/me", {
@@ -19,10 +31,10 @@ export default function Spotify() {
       });
 
       const userInfo = {
-          userId: data.id,
-          userName: data.display_name,
-        };
-        dispatch({ type: reducerCases.SET_USER, userInfo });
+        userId: data.id,
+        userName: data.display_name,
+      };
+      dispatch({ type: reducerCases.SET_USER, userInfo });
     };
     getUserInfo();
   }, [dispatch, token]);
@@ -30,10 +42,10 @@ export default function Spotify() {
     <Container>
       <div className="spotify__body">
         <Sidebar />
-        <div className="body">
-          <Navbar />
+        <div className="body" ref={bodyRef} onScroll={bodyScrolled}>
+          <Navbar navBackground={navBackground} />
           <div className="body__contents">
-            <Body />
+            <Body headerBackground={headerBackground} />
           </div>
         </div>
       </div>
@@ -61,5 +73,11 @@ const Container = styled.div`
     height: 100%;
     width: 100%;
     overflow: auto;
+    &::-webkit-scrollbar {
+      width: 0.7rem;
+      &-thumb {
+        background-color: rgba(255, 255, 255, 0.6);
+      }
+    }
   }
 `;
